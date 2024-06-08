2ff7ea5d540049d4ae25faae80511f53448229d9
jea-frvnco <163940309+jea-frvnco@users.noreply.github.com>
2024-06-08 11:17:49 -0400

    Update bucket.tf

--- bucket.tf
+++ bucket.tf
@@ -1,13 +1,59 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = "jean-bucket"
-  acl    = "private"
-
-  control_object_ownership = true
-  object_ownership         = "ObjectWriter"
-
-  versioning = {
-    enabled = true
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
   }
+}
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Creación del bucket
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket"
+
+  tags = {
+    Name = "My bucket"
+  }
+}
+
+# Permitir el acceso público al bucket
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.example.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+# Esperar antes de aplicar la política de acceso público
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.example.id
+
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [{
+      "Sid": "PublicRead",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": ["s3:GetObject"],
+      "Resource": [
+        "${aws_s3_bucket.example.arn}/*"
+      ]
+    }]
+  })
+
+  depends_on = [aws_s3_bucket_public_access_block.example]
+}
+
+# Crear objeto dentro del bucket
+resource "aws_s3_bucket_object" "object" {
+  bucket = aws_s3_bucket.example.id
+  key    = "index.html"
+  source = "index.html" # Esto asume que tienes un archivo index.html en tu directorio actual
+
+  depends_on = [aws_s3_bucket_policy.bucket_policy]
 }
